4a8b4b3626b2b5da960ce9751c3952d097e16fc0
gabribn99 <gabribn99@gmail.com>
2022-09-25 10:56:33 +0100

    Solucion errores

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
 import requests as r
 import time
 
-from bs4 import BeautifulSoup
-
-
 last_mail = None
 
 url = 'https://www.1secmail.com/api/v1/'
@@ -24,20 +21,21 @@ while (True):
     res = r.request("GET", url + get_messages).json()
 
     if len(res) > 0 and last_mail != res[0]:
-        #print(res[0])
+        # print(res[0])
         last_mail = res[0]
         get_data = f'&id={last_mail["id"]}'
         res = r.request("GET", url + read_message + get_data).json()
-        
+
         data = res['from']
         print('From: ' + data)
         data = res['subject']
         print('Subject: ' + data)
         data = res['body']
-        
-        print('Message: ')
-        bs = BeautifulSoup(data, 'html.parser')
-        body = bs.body.div
-        for linea in body.text.split('>'):
-            print(linea)
-            
+        print('Mensaje:')
+        for linea in data.split('<div'):
+            for lin in linea.split('</div>'):
+                lin = lin.replace('dir="ltr">','')
+                lin = lin.replace('<br>','\n')
+                lin = lin.replace('<','')
+                lin = lin.replace('>','')
+                print(lin)
